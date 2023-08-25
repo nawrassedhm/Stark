3664a38aa74f666d4352c65a2bab833c2b760f64
nawrassedhm <nawrassedhm@techie.com>
2023-08-25 19:45:46 +0000

    New commit

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,5 +2,5 @@ const { PREFIX } = require('../../config');
 
 module.exports = async bot => {
 console.log(`${bot.user.username} Is Available Now!`)
-bot.user.setPresence({ activity: { name: `music | ${PREFIX}help`, type: 'LISTENING' }, status: `idle` })
+bot.user.setPresence({ activity: { name: `Manifest S1 | ${PREFIX}help`, type: 'WATCHING' }, status: `dnd` })
 };
